1b9be5fbfd2d17f3ed23ed141dac3f0355e74c30
thanhthedev <huythanhdao@gmail.com>
2019-12-02 16:56:26 +1000

    now can register and login

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to timetotennis" }));
 
@@ -9,6 +15,6 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
